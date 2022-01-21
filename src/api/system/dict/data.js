3462bbec4a370365f5a2d005ad282d84c2060e2d
duziqiang <2760522291@qq.com>
2022-01-21 16:24:00 +0800

    :feat first int

---
+++ src/api/system/dict/data.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询字典数据列表
+export function listData(data) {
+  return request({
+    url: '/system/dict/data/list',
+    method: 'post',
+    data
+  })
+}
+
+// 查询字典数据详细
+export function getData(dictCode) {
+  return request({
+    url: '/system/dict/data/' + dictCode,
+    method: 'get'
+  })
+}
+
+// 根据字典类型查询字典数据信息
+export function getDicts(dictType) {
+  return request({
+    url: '/system/dict/data/type/' + dictType,
+    method: 'get'
+  })
+}
+
+// 新增字典数据
+export function addData(data) {
+  return request({
+    url: '/system/dict/data/saveOrUpdate',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改字典数据
+export function updateData(data) {
+  return request({
+    url: '/system/dict/data/saveOrUpdate',
+    method: 'post',
+    data: data
+  })
+}
+
+// 删除字典数据
+export function delData(data) {
+  return request({
+    url: '/system/dict/data/remove',
+    method: 'delete',
+    data
+  })
+}
